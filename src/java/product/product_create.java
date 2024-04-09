961050e63218d3fa138ff7bb3b7b7d8c89042f1e
Kavindu Dilshan <kdprojects847@gmail.com>
2024-04-09 14:52:17 +0100

    added the img file uploading feature

--- src/java/product/product_create.java
+++ src/java/product/product_create.java
@@ -1,18 +1,24 @@
     package product;
 
     import database.ConnectionPool;
+    import java.io.File;
+    import java.io.FileOutputStream;
     import java.io.IOException;
+    import java.io.InputStream;
     import java.sql.Connection;
     import java.sql.PreparedStatement;
     import java.sql.ResultSet;
+    import java.util.UUID;
     import static javax.faces.component.UIInput.isEmpty;
     import javax.servlet.ServletException;
+    import javax.servlet.annotation.MultipartConfig;
     import javax.servlet.annotation.WebServlet;
     import javax.servlet.http.HttpServlet;
     import javax.servlet.http.HttpServletRequest;
     import javax.servlet.http.HttpServletResponse;
+    import javax.servlet.http.Part;
 
-
+    @MultipartConfig
     @WebServlet(name = "product_create", urlPatterns = {"/product_create"})
     public class product_create extends HttpServlet {
 
@@ -27,18 +33,45 @@
             int quantity = ServletUtils.parseInt(request.getParameter("quantity_available"), 0);
             String size=request.getParameter("size");
             String discription=request.getParameter("discription");
-           
+            Part file=request.getPart("image");
+
             
             if(isEmpty(pname) || isEmpty(price) || isEmpty(category) || isEmpty(subcategory) || isEmpty(size) || isEmpty(discription) || isEmpty(quantity)){
                 response.getWriter().println("<h1>Add all the values in the feilds</h1>");
                 response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                return;
                 
             }
+//          file uploading
+            String imagename=file.getSubmittedFileName();
+                    
+//          Generate uuid for given image
+            String fileExtension = imagename.substring(imagename.lastIndexOf("."));
+            String uniqueID = UUID.randomUUID().toString();
+            String newImageName = uniqueID + fileExtension;
+            
+            
+            String uploadPath = "C:\\Users\\HP\\Documents\\NetbeansNewProjects\\JavaEcommerce\\web\\uploads\\"+ newImageName;
+            try {
+                FileOutputStream fos = new FileOutputStream(uploadPath);
+                InputStream is = file.getInputStream();
+
+                byte[] data = new byte[is.available()];
+                is.read(data);
+                fos.write(data);
+                fos.close();
+            } catch(IOException e) {
+                response.getWriter().println("<h1>error in adding images to uploads</h1>");
+                response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            }
+
+            
 
             String sql="Select * from admin where username=?";
             String sql2="insert into product(product_name,price,category_id,subcategory_id,size,description,quantity_available,admin_id)values(?,?,?,?,?,?,?,?)";
+            String sql3="insert into image(product_id,image_url) values(?,?)";
             
-            
+//            Insert product into database
             try(Connection connection=ConnectionPool.getConnection()){
                 
                 String usernames=(String)request.getAttribute("username");
@@ -67,7 +100,28 @@
                         int rs2=statement2.executeUpdate();
 
                         if(rs2>0){
-                            response.getWriter().println("<h1>product has added sucessfully</h1>"); 
+                            response.getWriter().println("<h1>product has added sucessfully to products</h1>");
+                            
+                            PreparedStatement statement3 = connection.prepareStatement("SELECT LAST_INSERT_ID()");
+                            ResultSet set=statement3.executeQuery();
+                            if(set.next()){
+                                int pId=set.getInt(1);
+                                PreparedStatement statement4=connection.prepareStatement(sql3);
+                                statement4.setInt(1, pId);
+                                statement4.setString(2, newImageName);
+                                int result=statement4.executeUpdate();
+                                
+                                if(result>0){
+                                    response.getWriter().println("<h1>product img added sucessfully</h1>");
+                                }else{
+                                    response.getWriter().println("<h1>product img added sucessfully </h1>");
+                                    response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+                                }
+                            }else{
+                                response.getWriter().println("<h1>cant get last inserted id</h1>");
+                                response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+                            }
+                            
                         }else{
                             response.getWriter().println("<h1>Cant add a peoduct</h1>");
                             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
